cc66158a75d12482f59ab97f5dd9efcb0130cf6a
eric-murat <emurat@free.fr>
2024-01-15 14:18:19 +0100

    ex03 rect

---
+++ ex03_rect/js/sketch.js
@@ -0,0 +1,29 @@
+function setup() {
+  createCanvas(550, 300);
+}
+
+function draw() {
+  background(220);
+  //describe('A white rectangle with a black outline on a gray canvas.');
+  rectMode(CORNER);
+  fill(255);
+  rect(50, 20, 100, 55);
+  // Texte noir taille 18
+  fill('black');
+  textSize(18);
+  text('rect(50, 20, 100, 55)', 180, 50);
+
+  fill('yellow');
+  rect(50, 100, 100, 55, 20);
+  // Texte noir taille 18
+  fill('black');
+  text('rect(50, 100, 100, 55, 20)', 180, 130);
+
+  fill('green');
+  rect(50, 200, 100, 55, 0, 10, 20, 30, 40);
+  // Texte noir taille 18
+  fill('black');
+  text('rect(50, 200, 100, 55, 0, 10, 20, 30, 40)', 180, 230);
+
+
+}
