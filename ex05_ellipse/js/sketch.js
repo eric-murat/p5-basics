0c7ce20db7ed6845353a95540f90d51b40d09790
eric-murat <emurat@free.fr>
2024-01-26 13:39:24 +0100

    Ajout ex05 ellipses

--- ex05_ellipse/js/sketch.js
+++ ex05_ellipse/js/sketch.js
@@ -6,23 +6,23 @@ function draw() {
   background(220);
   ellipseMode(CENTER);
   fill(255);
-  circle(100, 50, 50);
+  ellipse(100, 50, 50, 50);
     // Texte noir taille 18
   fill('black');
   textSize(18);
-  text('circle(100, 50, 50)', 180, 50);
+  text('ellipse(100, 50, 50, 50)', 180, 50);
 
   fill('yellow');
-  circle(100, 130, 100);
+  ellipse(100, 130, 100, 30);
   // Texte noir taille 18
   fill('black');
-  text('circle(100, 130, 100)', 180, 130);
+  text('ellipse(100, 130, 100, 30)', 180, 130);
 
   fill('green');
-  circle(100, 220, 10);
+  ellipse(100, 220, 20, 70);
   // Texte noir taille 18
   fill('black');
-  text('circle(100, 220, 10)', 180, 230);
+  text('ellipse(100, 220, 20, 70)', 180, 230);
 
 
 }
